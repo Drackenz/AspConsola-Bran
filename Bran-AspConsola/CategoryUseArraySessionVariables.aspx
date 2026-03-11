f518ca4209cae6bc9f8c4e9c6f907365c8be1101
Bran <branz7377@gmail.com>
2026-03-11 16:05:07 -0600

    Ej Array - Uso de categoria

---
+++ Bran-AspConsola/CategoryUseArraySessionVariables.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CategoryUseArraySessionVariables.aspx.cs" Inherits="Bran_AspConsola.CategoryUseArraySessionVariables" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Usar categoria array"></asp:Label>
+            <br />
+        </div>
+        <asp:TextBox ID="txtIndex" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnLookup" runat="server" OnClick="btnLookup_Click" Text="Lookup" />
+&nbsp;<asp:Label ID="lblCategory" runat="server" Text="Racquets"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Bran-AspConsola/CategoryUseArraySessionVariables.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bran_AspConsola
+{
+    public partial class CategoryUseArraySessionVariables : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLookup_Click(object sender, EventArgs e)
+        {
+            // Declare a new array.
+            String[] strCategoryArray = new String[5];
+
+            // Get the array from the session variable.
+            strCategoryArray = Session["strCategoryArray"] as String[];
+
+            // Use the array as normal.
+            Int32 i32Index = Convert.ToInt32(txtIndex.Text);
+
+            lblCategory.Text = strCategoryArray[i32Index];
+        }
+    }
+}
