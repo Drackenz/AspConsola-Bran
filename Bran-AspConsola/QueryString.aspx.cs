6d2f5a13a11326d3e726431c19f672196fed73e8
Bran <branz7377@gmail.com>
2026-03-11 15:48:13 -0600

    Ejercicio QueryString - formulario

---
+++ Bran-AspConsola/QueryString.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bran_AspConsola
+{
+    public partial class QueryString : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            // Go to the confirmation page with the encoded URL.
+            Response.Redirect(
+                "ProductConfirmQueryStrings.aspx" +
+                "?ddlCategory=" + ddlCategory.SelectedValue +
+                "&ddlSupplier=" + ddlSupplier.SelectedValue +
+                "&strProduct=" + txtProduct.Text +
+                "&strDescription=" + txtDescription.Text +
+                "&strImage=" + txtImage.Text +
+                "&decPrice=" + txtPrice.Text +
+                "&bytNumberInStock=" + txtNumberInStock.Text +
+                "&bytNumberOnOrder=" + txtNumberOnOrder.Text +
+                "&bytReorderLevel=" + txtReorderLevel.Text
+            );
+        }
+    }
+}
